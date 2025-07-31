3a6505074171511e8a05583bbfa0ba56ea4014d6
Benson-xix <bensondominic81@gmail.com>
2025-07-31 15:30:16 +0300

    update

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        url: 'http://https://qodebook-sass.onrender.com/',
       },
     ],
     components: {
