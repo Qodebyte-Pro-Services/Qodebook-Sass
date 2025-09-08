4619b1041891eaa2db1e387b2f25a73fbba098d6
Benson-xix <bensondominic81@gmail.com>
2025-09-08 08:04:18 +0300

    Add updates

--- src/controllers/salesController.js
+++ src/controllers/salesController.js
@@ -116,7 +116,7 @@ if (customerId === 0) {
     return res.status(201).json({ sale: order });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ message: 'Server error.' });
+    return res.status(500).json({ message: 'Server error.', details: err.message });
   }
 };
 
