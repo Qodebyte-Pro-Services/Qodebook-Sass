b594bcbb72f6ea1e2782c5b559c6b236bade6822
Benson-xix <bensondominic81@gmail.com>
2025-10-28 16:45:54 +0100

    first commit

--- src/controllers/staffController.js
+++ src/controllers/staffController.js
@@ -340,10 +340,13 @@ const result = await pool.query(`
     national_id, guarantor_name, guarantor_contact, guarantor_relationship,
     guarantor_address, photo, payment_status, last_payment_date,
     staff_status_change_reason, password_hash, password_changed_at
-  ) VALUES (
-    $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,
-    $20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30
-  ) RETURNING *;
+  )
+  VALUES (
+    $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,
+    $11,$12,$13,$14,$15,$16,$17,$18,$19,$20,
+    $21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31
+  )
+  RETURNING *;
 `, [
   staff_id, business_id, branch_id, full_name, contact_no, email,
   address, documentUrls, position_name, assigned_position, gender,
