a2efc4b04b2f213457099dbc287e7574e7b9e4c0
Benson-xix <bensondominic81@gmail.com>
2025-11-01 12:34:56 +0100

    first commit

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,12 +1,31 @@
 const multer = require('multer');
 
-const storage = multer.memoryStorage(); 
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) {
-    cb(null, true);
+ 
+  if (file.fieldname === 'photo') {
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('Photo must be an image!'), false);
+    }
+  }
+  
+  else if (file.fieldname === 'documents') {
+    if (
+      file.mimetype.startsWith('image/') ||
+      file.mimetype === 'application/pdf' ||
+      file.mimetype ===
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document' || 
+      file.mimetype === 'application/msword' 
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error('Documents must be images or PDFs/DOCXs!'), false);
+    }
   } else {
-    cb(new Error('Only image files are allowed!'), false);
+    cb(new Error('Unexpected field uploaded!'), false);
   }
 };
 
