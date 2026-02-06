957b9a9cce2e1ebd7c2ceef62ad08b18ca3801a9
Benson-xix <bensondominic81@gmail.com>
2026-02-06 10:55:31 +0100

    commit

--- src/controllers/staffController.js
+++ src/controllers/staffController.js
@@ -1121,7 +1121,7 @@ exports.staffLogin = async (req, res) => {
 
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ message: 'Server error.' });
+    return res.status(500).json({ message: 'Server error.', error: err.message });
   }
 };
 
