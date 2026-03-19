d90ce036706cfee055f6ce4cdbb912f6fec26967
Benson-xix <bensondominic81@gmail.com>
2026-03-19 10:33:47 +0100

    Fixxes

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ app.use(helmet());
 app.use(cors({
     origin: ['https://qode-book-main-sass.vercel.app', 'http://localhost:3000'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-business-id', 'x-branch-id'],
 }));
 
